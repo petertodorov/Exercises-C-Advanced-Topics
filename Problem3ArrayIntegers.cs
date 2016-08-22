9e29a0c81a97f1d22421a78470d1e762fe061731
petertodorov <petodorov@gmail.com>
2016-08-22 13:53:43 +0300

    Add files via upload

---
+++ Problem3ArrayIntegers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Problem3ArrayIntegers
+{
+    static void Main()
+    {
+        int[] myArray = new int[10];
+       Random number = new Random();
+        int sum = 0;
+        for (int i = 0; i <myArray.Length; i++)
+        {
+            myArray[i] = number.Next(0, 100);
+            sum += myArray[i];
+            Console.WriteLine(sum);
+        }
+    }
+}
+
