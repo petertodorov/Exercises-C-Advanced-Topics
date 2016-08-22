a3244ae09a7a7a88602c9057f0be74a993981604
petertodorov <petodorov@gmail.com>
2016-08-22 11:26:38 +0300

    Add files via upload
    Solution of the Problem 1

---
+++ Problem1PrintingNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class Problem1PrintingNumbers
+    {
+    static void PrintEvenNumbers(int minRange, int maxRange)
+    {
+        for (int i = minRange; i <= maxRange; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+
+    static void Main()
+    {
+        int minRange = int.Parse(Console.ReadLine());
+        int maxRange = int.Parse(Console.ReadLine());
+        PrintEvenNumbers(minRange, maxRange);
+    }
+}
+
