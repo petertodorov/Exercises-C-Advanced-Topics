f220c89359b3896dae1617368aac592586c80eb8
petertodorov <petodorov@gmail.com>
2016-08-22 15:52:36 +0300

    Add files via upload

---
+++ Problem4TextModification.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Problem4TextModification
+{
+    static void Main()
+    {
+        string sentence = "you though this deer was real…nope, just chuck testa.";
+        Console.WriteLine(sentence);
+        int[] number = new int[sentence.Length];
+        for (int i = 0; i < sentence.Length; i++)
+        {
+            number[i] = Convert.ToInt32(sentence[i]);
+            if (number[i] % 3 == 0)
+            {
+                int newNumber = number[i] - 32;
+                number[i] = newNumber;
+            }
+            Console.Write((char)(number[i]));
+        }
+    }
+}
+
